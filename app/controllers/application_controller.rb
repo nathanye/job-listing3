59a9381fcb0445c8326a7e9dcef77836f0a1fb3b
nathanye <khalil54781@gmail.com>
2017-04-11 16:04:45 +0800

    implement wage part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception|
+  protect_from_forgery with: :exception
 
   def require_is_admin
-    if current_user.admin?
+    if !current_user.admin?
       redirect_to root_path, alert: "你不是管理员，无权访问！"
+    end
   end
 end
