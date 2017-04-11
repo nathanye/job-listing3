563ed5b7021e0770b848a043ae26b453f6092232
nathanye <khalil54781@gmail.com>
2017-04-11 16:18:17 +0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -29,7 +29,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def update
-    @job = Job.find(params[:job])
+    @job = Job.find(params[:id])
 
     if @job.update(job_params)
       redirect_to admin_jobs_path, notice: "职位已更新！"
@@ -47,6 +47,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 end
